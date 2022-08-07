2da57318da16184ad9b6c330b2f671785eea07f4
Jeff Reinecke <jeff@paploo.net>
2022-08-07 13:23:56 -0700

    Setup for making IDs predictable by linkage rather than values; this makes syncing way easier

--- ruby/lib/goi/model/vocabulary.rb
+++ ruby/lib/goi/model/vocabulary.rb
@@ -92,6 +92,12 @@ module Goi
 
         UUID5_NAMESPACE = UUIDTools::UUID.parse('c7812647-678a-4bf5-bed3-b33fe499469c').to_s
 
+        def self.create_id_in_linkage(vocabulary_id, linkage_field)
+          name = [vocabulary_id, linkage_field].map(&:to_s).join('|')
+          ns = UUIDTools::UUID.parse(UUID5_NAMESPACE)
+          UUIDTools::UUID.sha1_create(ns, name).to_s
+        end
+
         def self.create_id(vocabulary_id, value)
           name = [vocabulary_id, value].map(&:to_s).join('|')
           ns = UUIDTools::UUID.parse(UUID5_NAMESPACE)
@@ -102,8 +108,8 @@ module Goi
           new(**attributes)
         end
 
-        def initialize(vocabulary_id:, value:, sort_rank: 1)
-          @id = self.class.create_id(vocabulary_id, value)
+        def initialize(id: nil,vocabulary_id:, value:, sort_rank: 1)
+          @id = id || self.class.create_id(vocabulary_id, value)
           @vocabulary_id = vocabulary_id
           @value = value
           @sort_rank = sort_rank
@@ -122,6 +128,16 @@ module Goi
 
         UUID5_NAMESPACE = UUIDTools::UUID.parse('546a4b2c-6b83-4fe9-902e-7c7ade990930').to_s
 
+        # Create an ID that is consistent for the linkage position.
+        # This makes database syncing scripts a LOT easier to manage.
+        def self.create_id_in_linkage(vocabulary_id, linkage_field)
+          name = [vocabulary_id, linkage_field].map(&:to_s).join('|')
+          ns = UUIDTools::UUID.parse(UUID5_NAMESPACE)
+          UUIDTools::UUID.sha1_create(ns, name).to_s
+        end
+
+        # Create an ID that is consistent for a given set of fields.
+        # This makes database syncing scripts hard because any updates to the values change the ID!
         def self.create_id(vocabulary_id, spelling_kind_code, value)
           name = [vocabulary_id, spelling_kind_code, value].map(&:to_s).join('|')
           ns = UUIDTools::UUID.parse(UUID5_NAMESPACE)
@@ -132,8 +148,8 @@ module Goi
           new(**attributes)
         end
 
-        def initialize(vocabulary_id:, spelling_kind_code:, value:)
-          @id = self.class.create_id(vocabulary_id, spelling_kind_code, value)
+        def initialize(id: nil,vocabulary_id:, spelling_kind_code:, value:)
+          @id = id || self.class.create_id(vocabulary_id, spelling_kind_code, value)
           @vocabulary_id = vocabulary_id
           @spelling_kind_code = spelling_kind_code
           @value = value
