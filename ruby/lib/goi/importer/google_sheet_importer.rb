9476d5d1010f715afe4c12268c508dd0abd290b5
Jeff Reinecke <jeff@paploo.net>
2022-08-07 13:47:13 -0700

    Moved the google sheet importer's helper classes into the class to namespace safely

--- ruby/lib/goi/importer/google_sheet_importer.rb
+++ ruby/lib/goi/importer/google_sheet_importer.rb
@@ -59,94 +59,94 @@ module Goi
         spelling_parser.parse_row(row:, field_data:, required:, vocabulary_id:)
       end
 
-    end
+      class VocabularyParser
 
-    class VocabularyParser
-
-      def parse_row(row:)
-        Goi::Model::Vocabulary::Vocabulary.new(
-          id: row[ID_KEY]&.clean,
-          word_class_code: row.fetch_required(WORD_CLASS_CODE_KEY).clean,
-          conjugation_kind_code: row[CONJUGATION_KIND_CODE_KEY]&.clean,
-          jlpt_level: row[JLPT_LEVEL_KEY]&.clean&.to_i,
-          row_num: row.fetch_required(ROW_NUM_KEY).clean.to_i,
-          date_added: row.fetch(DATE_ADDED_KEY)&.clean&.then { |s| Date.parse(s) },
-          tags: array_parse(row[TAGS_KEY]&.clean),
-          lesson_codes: array_parse(row[LESSON_CODES_KEY]&.clean)
-        )
-      end
-
-      private
-
-      ID_KEY = 'id'.freeze
-      WORD_CLASS_CODE_KEY = 'word_class_code'.freeze
-      CONJUGATION_KIND_CODE_KEY = 'conjugation_kind_code'.freeze
-      JLPT_LEVEL_KEY = 'jlpt_level'.freeze
-      ROW_NUM_KEY = 'row_num'.freeze
-      DATE_ADDED_KEY = 'date_added'.freeze
-      TAGS_KEY = 'tags'.freeze
-      LESSON_CODES_KEY = 'lesson_codes'.freeze
-
-      def array_parse(value)
-        value&.clean&.delete('{}')&.split(/\s+|,\s*/) || []
-      end
-
-      def const_case(s)
-        # TODO: Make this also turn any non-alphanum char to an underscore.
-        s.upcase
-      end
-
-    end
-
-    class DefinitionParser
-
-      DEFINITION_KEY = 'definition'.freeze
-
-      def parse_row(row:, vocabulary_id:)
-        Goi::Model::Vocabulary::Definition.new(
-          id: Goi::Model::Vocabulary::Definition.create_id(vocabulary_id:, linkage_field: :preferred_definition),
-          vocabulary_id:,
-          value: row.fetch_required(DEFINITION_KEY).clean
-        )
-      end
-
-    end
-
-    class SpellingParser
-
-      CODE_MAP = {
-        hiragana: 'HIRAGANA',
-        kanji: 'KANJI',
-        katakana: 'KATAKANA'
-      }.freeze
-
-      def parse_row(row:, field_data:, required: true, vocabulary_id:)
-        key = field_data.fetch(:key)
-        value = row[key]&.clean
-
-        if value.nil?
-          raise(ArgumentError, "Missing required spelling for key #{key.inspect} from row #{row.inspect}") if required
-          return nil
+        def parse_row(row:)
+          Goi::Model::Vocabulary::Vocabulary.new(
+            id: row[ID_KEY]&.clean,
+            word_class_code: row.fetch_required(WORD_CLASS_CODE_KEY).clean,
+            conjugation_kind_code: row[CONJUGATION_KIND_CODE_KEY]&.clean,
+            jlpt_level: row[JLPT_LEVEL_KEY]&.clean&.to_i,
+            row_num: row.fetch_required(ROW_NUM_KEY).clean.to_i,
+            date_added: row.fetch(DATE_ADDED_KEY)&.clean&.then { |s| Date.parse(s) },
+            tags: array_parse(row[TAGS_KEY]&.clean),
+            lesson_codes: array_parse(row[LESSON_CODES_KEY]&.clean)
+          )
         end
 
-        linkage_field = field_data.fetch(:linkage_field)
-        id = Goi::Model::Vocabulary::Spelling.create_id(vocabulary_id:, linkage_field:)
+        private
 
-        Goi::Model::Vocabulary::Spelling.new(
-          id:,
-          vocabulary_id:,
-          spelling_kind_code: infer_kind_code(value),
-          value:
-        )
+        ID_KEY = 'id'.freeze
+        WORD_CLASS_CODE_KEY = 'word_class_code'.freeze
+        CONJUGATION_KIND_CODE_KEY = 'conjugation_kind_code'.freeze
+        JLPT_LEVEL_KEY = 'jlpt_level'.freeze
+        ROW_NUM_KEY = 'row_num'.freeze
+        DATE_ADDED_KEY = 'date_added'.freeze
+        TAGS_KEY = 'tags'.freeze
+        LESSON_CODES_KEY = 'lesson_codes'.freeze
+
+        def array_parse(value)
+          value&.clean&.delete('{}')&.split(/\s+|,\s*/) || []
+        end
+
+        def const_case(s)
+          # TODO: Make this also turn any non-alphanum char to an underscore.
+          s.upcase
+        end
+
       end
 
-      private
+      class DefinitionParser
 
-      def infer_kind_code(value)
-        raw_class = Goi::Nihongo::StringClassification.classify_string(value)
-        CODE_MAP.fetch(raw_class) do |k|
-          raise(ArgumentError, "Cannot code illegal classification '#{k.inspect}' for string #{value.inspect}")
+        DEFINITION_KEY = 'definition'.freeze
+
+        def parse_row(row:, vocabulary_id:)
+          Goi::Model::Vocabulary::Definition.new(
+            id: Goi::Model::Vocabulary::Definition.create_id(vocabulary_id:, linkage_field: :preferred_definition),
+            vocabulary_id:,
+            value: row.fetch_required(DEFINITION_KEY).clean
+          )
         end
+
+      end
+
+      class SpellingParser
+
+        CODE_MAP = {
+          hiragana: 'HIRAGANA',
+          kanji: 'KANJI',
+          katakana: 'KATAKANA'
+        }.freeze
+
+        def parse_row(row:, field_data:, required: true, vocabulary_id:)
+          key = field_data.fetch(:key)
+          value = row[key]&.clean
+
+          if value.nil?
+            raise(ArgumentError, "Missing required spelling for key #{key.inspect} from row #{row.inspect}") if required
+            return nil
+          end
+
+          linkage_field = field_data.fetch(:linkage_field)
+          id = Goi::Model::Vocabulary::Spelling.create_id(vocabulary_id:, linkage_field:)
+
+          Goi::Model::Vocabulary::Spelling.new(
+            id:,
+            vocabulary_id:,
+            spelling_kind_code: infer_kind_code(value),
+            value:
+          )
+        end
+
+        private
+
+        def infer_kind_code(value)
+          raw_class = Goi::Nihongo::StringClassification.classify_string(value)
+          CODE_MAP.fetch(raw_class) do |k|
+            raise(ArgumentError, "Cannot code illegal classification '#{k.inspect}' for string #{value.inspect}")
+          end
+        end
+
       end
 
     end
