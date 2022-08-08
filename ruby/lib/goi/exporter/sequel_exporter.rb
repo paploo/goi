e40b65615bfa0870d5524f0b91a237f0bb8f1da7
Jeff Reinecke <jeff@paploo.net>
2022-08-08 00:02:27 -0700

    Removed excess code from sql file importer, moved to be a comment in the sequel exporter for when that is revisited

--- ruby/lib/goi/exporter/sequel_exporter.rb
+++ ruby/lib/goi/exporter/sequel_exporter.rb
@@ -2,3 +2,79 @@
 #
 # This is the hardest one because we either have to work out a sync
 # strategy, or delete whole words and re-add them in a transaction.
+
+
+# class Synchronizer
+#
+#   # To sync we must:
+#   # - Identify records to insert,
+#   # - Identify records to remove,
+#   # - Identify records to update.
+#   #
+#   # * For the vocabulary, definition, spellings and links, we assume ID idempotency based on relationship.
+#   # * For the references we must go through the same sync actions.
+#   def initialize(db:)
+#     @db = db
+#   end
+#
+#   def sync(record_groups)
+#     # First determine what to insert, update, and remove.
+#     db.transaction do
+#       library = Library.new(db:).load_all
+#       insert, update, delete = calculate_diff(library.vocabulary_ids, record_groups.map { |g| g[:vocabulary][:id ]})
+#     end
+#     raise NotImplementedError
+#   end
+#
+#   private
+#
+#   attr_reader :db
+#
+#   def calculate_diff(old_values, new_values)
+#     [
+#       #insert
+#       new_values - old_values,
+#       #update
+#       new_values.intersection(old_values),
+#       #delete
+#       old_values - new_values
+#     ]
+#   end
+#
+#   class Library
+#
+#     def initialize(db:)
+#       @db = db
+#     end
+#
+#     attr_reader :vocabulary_ids
+#     attr_reader :reference_codes
+#
+#     def load_all
+#       load_vocabulary
+#       load_references
+#
+#       self
+#     end
+#
+#     private
+#
+#     def load_vocabulary
+#       @vocabulary_ids = db[Sequel[:vocabulary][:vocabulary]]
+#                           .select(:id)
+#                           .all
+#                           .map { |r| r[:id] }
+#     end
+#
+#     def load_references
+#       @references = db[Sequel[:vocabulary][:reference]]
+#                       .all
+#                       .group_by { |r| r[:vocabulary_id] }
+#                       .transform_values { |v| v.map { |r| r[:lesson_code] } }
+#     end
+#
+#   end
+#
+#   attr_reader :db
+#
+# end
--- ruby/lib/goi/exporter/sql_file_exporter.rb
+++ ruby/lib/goi/exporter/sql_file_exporter.rb
@@ -4,11 +4,10 @@ module Goi
   module Exporter
     class SqlFileExporter < BaseExporter
 
-      def initialize(mode: :sql)
+      def initialize
         super()
         @db = Sequel.postgres('goi', user: 'postgres', password: 'postgres', host: 'localhost')
         @io = STDOUT
-        @mode = mode
       end
 
       attr_reader :db
@@ -21,13 +20,9 @@ module Goi
           record_builder.build_record_group(ln)
         end
 
-        if mode == :sql
-          record_groups.each do |record_group|
-            sql_stmts = generate_insert_sql(record_group)
-            sql_stmts.each { |stmt| io.puts(stmt + ';') }
-          end
-        else
-          Synchronizer.new(db:).sync(record_group)
+        record_groups.each do |record_group|
+          sql_stmts = generate_insert_sql(record_group)
+          sql_stmts.each { |stmt| io.puts(stmt + ';') }
         end
       end
 
@@ -50,81 +45,6 @@ module Goi
         end
 
         main_entities_sql + ref_group_sql
-      end
-
-      class Synchronizer
-
-        # To sync we must:
-        # - Identify records to insert,
-        # - Identify records to remove,
-        # - Identify records to update.
-        #
-        # * For the vocabulary, definition, spellings and links, we assume ID idempotency based on relationship.
-        # * For the references we must go through the same sync actions.
-        def initialize(db:)
-          @db = db
-        end
-
-        def sync(record_groups)
-          # First determine what to insert, update, and remove.
-          db.transaction do
-            library = Library.new(db:).load_all
-            insert, update, delete = calculate_diff(library.vocabulary_ids, record_groups.map { |g| g[:vocabulary][:id ]})
-          end
-          raise NotImplementedError
-        end
-
-        private
-
-        attr_reader :db
-
-        def calculate_diff(old_values, new_values)
-          [
-            #insert
-            new_values - old_values,
-            #update
-            new_values.intersection(old_values),
-            #delete
-            old_values - new_values
-          ]
-        end
-
-        class Library
-
-          def initialize(db:)
-            @db = db
-          end
-
-          attr_reader :vocabulary_ids
-          attr_reader :reference_codes
-
-          def load_all
-            load_vocabulary
-            load_references
-
-            self
-          end
-
-          private
-
-          def load_vocabulary
-            @vocabulary_ids = db[Sequel[:vocabulary][:vocabulary]]
-                                .select(:id)
-                                .all
-                                .map { |r| r[:id] }
-          end
-
-          def load_references
-            @references = db[Sequel[:vocabulary][:reference]]
-                            .all
-                            .group_by { |r| r[:vocabulary_id] }
-                            .transform_values { |v| v.map { |r| r[:lesson_code] } }
-          end
-
-        end
-
-        attr_reader :db
-
       end
 
       class VocabularyRecordBuilder
