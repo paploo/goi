dc2a80ff11ede3c540ab21d6ac8fba396b699f8c
Jeff Reinecke <jeff@paploo.net>
2022-09-07 19:12:55 -0700

    Add the rest of GENKI L5

--- db/data/V1__lookup_tables.sql
+++ db/data/V1__lookup_tables.sql
@@ -15,6 +15,7 @@ INSERT INTO vocabulary.word_class (code, label) VALUES ('EXPRESSION', 'Expressio
 INSERT INTO vocabulary.word_class (code, label) VALUES ('CONTRACTION', 'Contraction');
 INSERT INTO vocabulary.word_class (code, label) VALUES ('COUNTER', 'Counter');
 INSERT INTO vocabulary.word_class (code, label) VALUES ('SUFFIX', 'Suffix');
+INSERT INTO vocabulary.word_class (code, label) VALUES ('PRE_NOUN_ADJECTIVAL', 'Pre-Noun Adjectival')
 
 ---
 --- CONJUGATION KIND
