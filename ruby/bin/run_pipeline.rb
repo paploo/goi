6b027321a17a413af0a2417f6eb9d67f52b4a0cb
Jeff Reinecke <jeff@paploo.net>
2023-03-08 00:10:03 -0800

    Refactored bin/run_pipeline to have run configurations that can be easily selected between

--- ruby/bin/run_pipeline.rb
+++ ruby/bin/run_pipeline.rb
@@ -2,42 +2,29 @@
 
 require 'pathname'
 
-require_relative '../lib/goi/pipeline'
+require_relative '../lib/goi/application/pipeline_app'
 
-# TODO: Make general for any pipeline and put in goi library; then we can have multiple CLI apps if we want with minimal code.
-module Goi
-  module Bin
-    module Pipeline
+RUN_CONFIGURATIONS = {
+  vocabulary: {
+    pipeline_identifier: :vocabulary_pipeline,
+    pipeline_config: Goi::Pipeline::Factory::Config.new(
+      db_config: { database: 'goi', user: 'postgres', password: 'postgres', host: 'localhost' },
+      infile_pathname: Pathname(__FILE__).expand_path.join('..', '..', '..', 'files', '日本語 Vocab - Vocab.csv'),
+      output_dir_pathname: Pathname(__FILE__).expand_path.join('..', '..', '..', 'files', 'vocabulary')
+    )
+  },
+  grammar: {
+    pipeline_identifier: :grammar_pipeline,
+    pipeline_config: Goi::Pipeline::Factory::Config.new(
+      db_config: { database: 'goi', user: 'postgres', password: 'postgres', host: 'localhost' },
+      infile_pathname: Pathname(__FILE__).expand_path.join('..', '..', '..', 'files', '日本語 Vocab - Grammar.json'),
+      output_dir_pathname: Pathname(__FILE__).expand_path.join('..', '..', '..', 'files', 'grammar')
+    )
+  }
+}.freeze
 
-      class Application
+#TODO: Select the configuration based on an argument
+#TODO: Have an :all mode that runs through all pipeline run configurations.
+run_configuration = RUN_CONFIGURATIONS[:grammar]
 
-        def initialize(config:)
-          @pipeline_factory = Goi::Pipeline::Factory.new(config:)
-        end
-
-        attr_reader :pipeline_factory
-
-        def run
-          #pipeline_factory.vocabulary_pipeline.run
-          pipeline_factory.grammar_pipeline.run
-        end
-
-      end
-
-    end
-  end
-end
-
-# config = Goi::Pipeline::Factory::Config.new(
-#   db_config: { database: 'goi', user: 'postgres', password: 'postgres', host: 'localhost' },
-#   infile_pathname: Pathname(__FILE__).expand_path.join('..', '..', '..', 'files', '日本語 Vocab - Vocab.csv'),
-#   output_dir_pathname: Pathname(__FILE__).expand_path.join('..', '..', '..', 'files', 'vocabulary')
-# )
-
-config = Goi::Pipeline::Factory::Config.new(
-  db_config: { database: 'goi', user: 'postgres', password: 'postgres', host: 'localhost' },
-  infile_pathname: Pathname(__FILE__).expand_path.join('..', '..', '..', 'files', '日本語 Vocab - Grammar.json'),
-  output_dir_pathname: Pathname(__FILE__).expand_path.join('..', '..', '..', 'files', 'grammar')
-)
-
-Goi::Bin::Pipeline::Application.new(config:).run
+Goi::Application::PipelineApp.new(**run_configuration).run
