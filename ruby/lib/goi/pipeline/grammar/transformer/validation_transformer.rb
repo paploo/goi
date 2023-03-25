2eba6d4335afc52cd365b6cb751cb61480419611
Jeff Reinecke <jeff@paploo.net>
2023-03-25 01:36:36 -0700

    Added Genki L5 grammar lessons; added JLPT 先生 sources, and added some grammar rule validation messages for common errors from copy/pasting templates

--- ruby/lib/goi/pipeline/grammar/transformer/validation_transformer.rb
+++ ruby/lib/goi/pipeline/grammar/transformer/validation_transformer.rb
@@ -27,6 +27,9 @@ module Goi
 
           private
 
+          EMPTY_UUID = "00000000-0000-0000-0000-000000000000"
+          MIN_CREATE_DATE = Date.new(2020, 1, 1)
+
           def io = $stderr
 
           def report(hydrated_rules:)
@@ -34,9 +37,12 @@ module Goi
               title: 'Validation Report',
               messages: [
                 duplicate_rule_ids(hydrated_rules:),
+                empty_rule_ids(hydrated_rules:),
                 duplicate_example_ids(hydrated_rules:),
+                empty_example_ids(hydrated_rules:),
                 rule_title_placeholder_case(hydrated_rules:),
                 rule_meaning_placeholder_case(hydrated_rules:),
+                invalid_date_added(hydrated_rules:),
                 missing_examples(hydrated_rules:)
               ].compact.filter { |r| !r.empty? }
             )
@@ -70,6 +76,29 @@ module Goi
             )
           end
 
+          def empty_rule_ids(hydrated_rules:)
+            has_empty = hydrated_rules.any? { |hr| hr.rule.id.nil? || hr.rule.id == EMPTY_UUID }
+            return nil unless has_empty
+
+            Goi::Core::ValidationReport.new(
+              title: "Empty Rule IDs",
+              messages: [Goi::Core::ValidationMessage.error("One or more rules have IDs that are null or #{EMPTY_UUID}")]
+            )
+          end
+
+          def empty_example_ids(hydrated_rules:)
+            has_empty = hydrated_rules.any? do |hr|
+              hr.examples.any? { |ex| ex.id.nil? || ex.id == EMPTY_UUID }
+            end
+
+            return nil unless has_empty
+
+            Goi::Core::ValidationReport.new(
+              title: "Empty Example IDs",
+              messages: [Goi::Core::ValidationMessage.error("One or more rules have examples with IDs that are null or #{EMPTY_UUID}")]
+            )
+          end
+
           def rule_title_placeholder_case(hydrated_rules:)
             mistitled_hydrated_rules = hydrated_rules.select { |hr| hr.rule.title.preferred_spelling =~ /[A-Z]/}
             return nil if mistitled_hydrated_rules.empty?
@@ -94,6 +123,20 @@ module Goi
 
             Goi::Core::ValidationReport.new(
               title: 'Rule Meanings: Expect NO Full-Width characters',
+              messages: messages
+            )
+          end
+
+          def invalid_date_added(hydrated_rules:)
+            invalid = hydrated_rules.select { |hr| hr.rule.date_added.nil? || hr.rule.date_added < MIN_CREATE_DATE }
+            return nil if invalid.empty?
+
+            messages = invalid.map do |hr|
+              Goi::Core::ValidationMessage.error("Rule #{hr.rule.id} has an invalid date added: #{hr.rule.date_added&.to_s}")
+            end
+
+            Goi::Core::ValidationReport.new(
+              title: 'Rule Date Added Invalid',
               messages: messages
             )
           end
