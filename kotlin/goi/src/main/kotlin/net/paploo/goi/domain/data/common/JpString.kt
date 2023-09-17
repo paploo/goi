c0ebb97091022e46d4a1007c244ffdb574c9f840
Jeff Reinecke <jeff@paploo.net>
2023-09-16 19:46:24 -0700

    WIP: FuriganaTemplate

--- kotlin/goi/src/main/kotlin/net/paploo/goi/domain/data/common/JpString.kt
+++ kotlin/goi/src/main/kotlin/net/paploo/goi/domain/data/common/JpString.kt
@@ -1,11 +1,18 @@
 package net.paploo.goi.domain.data.common
 
 import net.paploo.goi.common.interfaces.Valued
+import net.paploo.goi.domain.tools.furiganatemplate.FuriganaTemplate
+
+//TODO: It seems like I might've mixed a couple concepts in the Ruby code. Reqs:
+// - We have to be able to get a preferred and phonetic spellings.
+// - We also have a need to convert to a template that can get converted to various template outputs.
+// - We can instantiate as either a preferred/phonetic and derive a template and derive the spellings.
+// -
 
 sealed interface JpString : Valued<String> {
     val preferredSpelling: Spelling
     val phoneticSpelling: Spelling
-    val furiganaSpelling: Furigana
+    val furiganaTemplate: FuriganaTemplate
 
     override val value: String get() = preferredSpelling.value
 
@@ -14,28 +21,21 @@ sealed interface JpString : Valued<String> {
         override val phoneticSpelling: Spelling
     ) : JpString {
 
-        override val furiganaSpelling: Furigana
-            get() = Furigana(preferredSpelling = preferredSpelling, phoneticSpelling = phoneticSpelling)
+        override val furiganaSpelling: FuriganaTemplate
+            get() = FuriganaTemplate(preferredSpelling = preferredSpelling, phoneticSpelling = phoneticSpelling)
 
     }
 
     data class Furigana(
-        val template: String
+        val template: FuriganaTemplate
     ) : JpString {
 
         override val preferredSpelling: Spelling = TODO("Parse from template")
 
         override val phoneticSpelling: Spelling = TODO("Parse from template")
 
-        override val furiganaSpelling: Furigana = this
-
-        companion object {
-
-            operator fun invoke(preferredSpelling: Spelling, phoneticSpelling: Spelling): Furigana = TODO("Build template from parts")
-
-        }
+        override val furiganaTemplate: FuriganaTemplate = template
 
     }
-
 
 }
